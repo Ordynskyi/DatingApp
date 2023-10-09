edecd6c310a29ef603c8eab0de22da2edb0aea71
Andrey Ordynsky <andrey.ordinskiy@deuscraft.com>
2023-10-08 17:33:54 -0700

    Initial commit

---
+++ API/Controllers/UsersController.cs
@@ -0,0 +1,30 @@
+﻿using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers;
+
+[ApiController]
+[Route("api/[controller]")] // /api/users
+public class UsersController : ControllerBase
+{
+    private readonly DataContext _contex;
+
+    public UsersController(DataContext contex)
+    {
+        _contex = contex;
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
+    {
+        return await _contex.Users.ToListAsync();
+    }
+
+    [HttpGet("{id}")]   // /api/users/2
+    public async Task<ActionResult<AppUser>> GetUser(int id)
+    {
+        return await _contex.Users.FindAsync(id);
+    }
+}
