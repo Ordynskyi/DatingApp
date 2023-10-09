122a29728521a28645751c6e82fcd9b29f9c77da
Andrey Ordynsky <andreyordynsky@gmail.com>
2023-10-09 14:36:36 -0700

    login and password required

--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -2,6 +2,16 @@
 
 public class AppUser
 {
+    public AppUser(int id, string userName, byte[] passwordHash, byte[] passwordSalt)
+    {
+        Id = id;
+        UserName = userName;
+        PasswordHash = passwordHash;
+        PasswordSalt = passwordSalt;
+    }
+
     public int Id { get; set; }
-    public string? UserName { get; set;}
+    public string UserName { get; set;}
+    public byte[] PasswordHash { get; set; }
+    public byte[] PasswordSalt { get; set; }
 }
