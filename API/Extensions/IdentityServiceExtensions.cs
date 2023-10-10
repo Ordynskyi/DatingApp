9923e3091f122209cc2028203ade57b2d9bdc7ec
Andrey Ordynsky <andreyordynsky@gmail.com>
2023-10-09 19:22:55 -0700

    Services binding moved to extensions

---
+++ API/Extensions/IdentityServiceExtensions.cs
@@ -0,0 +1,28 @@
+﻿using API.Interfaces;
+using API.Services;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+
+namespace API.Extensions;
+
+public static class IdentityServiceExtensions
+{
+    public static IServiceCollection AddIdentityServices(this IServiceCollection services,
+        IConfiguration config)
+    {
+        services.AddScoped<ISecurityKeyService, SymmetricKeyService>();
+        services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+            .AddJwtBearer(options => 
+            {
+                options.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuerSigningKey = true,
+                    IssuerSigningKey = new SymmetricKeyService(config).GetSecurityKey(), // TODO: to think about a better solution
+                    ValidateIssuer = false,
+                    ValidateAudience = false,
+                };
+            });
+
+        return services;
+    }
+}
