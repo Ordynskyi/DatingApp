b31fd6760f468bfed06a56ed0c8326c36c966d42
Andrey Ordynsky <andreyordynsky@gmail.com>
2023-10-20 20:39:57 -0700

    reactive forms for the register component

--- client/src/app/register/register.component.ts
+++ client/src/app/register/register.component.ts
@@ -1,6 +1,7 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { AccountService } from '../_services/account.service';
 import { ToastrService } from 'ngx-toastr';
+import { AbstractControl, FormBuilder, FormControl, FormGroup, ValidatorFn, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -8,22 +9,51 @@ import { ToastrService } from 'ngx-toastr';
   styleUrls: ['./register.component.css']
 })
 
-export class RegisterComponent {
+export class RegisterComponent implements OnInit {
   @Output() cancelRegister = new EventEmitter();
   model: any = {};
+  registerForm: FormGroup = new FormGroup({});
 
-  constructor(private accountService:AccountService,
-    private toastr: ToastrService) {}
+  constructor(
+    private accountService:AccountService,
+    private toastr: ToastrService,
+    private fb: FormBuilder) {}
+  
+  ngOnInit(): void {
+    this.initializeForm();
+  }
+
+  initializeForm(): void {
+    this.registerForm = this.fb.group({
+      username: ['', Validators.required],
+      password: ['', 
+        [Validators.required, 
+         Validators.minLength(4),
+         Validators.maxLength(64)]],
+      confirmPassword: ['', [Validators.required, this.matchValues('password')]],
+      });
+    
+    this.registerForm.controls['password'].valueChanges.subscribe({
+      next: () => this.registerForm.controls['confirmPassword'].updateValueAndValidity() 
+    });
+  }
+
+  matchValues(matchTo: string): ValidatorFn {
+    return (control: AbstractControl) => {
+      return control.value === control.parent?.get(matchTo)?.value ? null : {notMatching: true}
+    }
+  }
 
   register() {
-    this.accountService.register(this.model).subscribe({
-      next: () => this.cancel(),
-      error: error => {
+    console.log(this.registerForm?.value);
+    // this.accountService.register(this.model).subscribe({
+    //   next: () => this.cancel(),
+    //   error: error => {
 
-        console.log(error);
-        this.toastr.error(error);
-      }
-    });
+    //     console.log(error);
+    //     this.toastr.error(error);
+    //   }
+    // });
   }
 
   cancel() {
