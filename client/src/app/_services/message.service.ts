fefea50c565bd428e5642ba0b4f8572aa2ae6ed3
Andrey Ordynsky <andreyordynsky@gmail.com>
2023-10-23 11:51:31 -0700

    Client messages

--- client/src/app/_services/message.service.ts
+++ client/src/app/_services/message.service.ts
@@ -19,4 +19,14 @@ export class MessageService {
     params = params.append('Container', container);
     return getPaginatedResult<Message[]>(`${this.baseUrl}messages`, params, this.http);
   }
+
+  getMessageThread(username: string) {
+    return this.http.get<Message[]>(this.baseUrl + 'messages/thread/' + username);
+  }
+
+  sendMessage(username: string, content: string) {
+    return this.http.post<Message>(
+      this.baseUrl + 'messages', 
+      {recipientUsername: username, content});
+  }
 }
