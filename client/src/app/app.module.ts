ba7a6a09efe25bb1c96bbec1c95ffdbdeac0d53e
Andrey Ordynsky <andreyordynsky@gmail.com>
2023-10-24 12:15:34 -0700

    client directive hasRole for admin nav button

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -24,6 +24,7 @@ import { PhotoEditorComponent } from './members/photo-editor/photo-editor.compon
 import { TextInputComponent } from './_forms/text-input/text-input.component';
 import { DatePickerComponent } from './_forms/date-picker/date-picker.component';
 import { AdminPanelComponent } from './admin/admin-panel/admin-panel.component';
+import { HasRoleDirective } from './_directives/has-role.directive';
 
 @NgModule({
   declarations: [
@@ -42,6 +43,7 @@ import { AdminPanelComponent } from './admin/admin-panel/admin-panel.component';
     TextInputComponent,
     DatePickerComponent,
     AdminPanelComponent,
+    HasRoleDirective,
   ],
   imports: [
     BrowserModule,
