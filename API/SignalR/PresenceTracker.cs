ee689215d4ecc27468a824e8e055e60ff6c07908
Andrey Ordynsky <andreyordynsky@gmail.com>
2023-10-24 14:58:32 -0700

    API presence tracker

---
+++ API/SignalR/PresenceTracker.cs
@@ -0,0 +1,50 @@
+﻿namespace API.SignalR;
+
+public class PresenceTracker
+{
+    private readonly Dictionary<string, List<string>> OnlineUsers = new ();
+
+    public Task UserConnected(string username, string connectionId)
+    {
+        lock(OnlineUsers)
+        {
+            if (!OnlineUsers.TryGetValue(username, out var connectionIds))
+            {
+                connectionIds = new List<string>();
+                OnlineUsers[username] = connectionIds;
+            }
+
+            connectionIds.Add(connectionId);
+        }
+
+        return Task.CompletedTask;
+    }
+
+    public Task UserDisconnected(string username, string connectionId)
+    {
+        lock(OnlineUsers)
+        {
+            if (!OnlineUsers.TryGetValue(username, out var connectionIds)) return Task.CompletedTask;
+
+            connectionIds.Remove(connectionId);
+            if (connectionIds.Count == 0)
+            {
+                OnlineUsers.Remove(username);
+            }
+        }
+
+        return Task.CompletedTask;
+    }
+
+    public Task<string[]> GetOnlineUsers()
+    {
+        string[] usernames;
+        lock(OnlineUsers)
+        {
+            usernames = OnlineUsers.Keys.ToArray();
+        }
+
+        Array.Sort(usernames);
+        return Task.FromResult(usernames);
+    }
+}
