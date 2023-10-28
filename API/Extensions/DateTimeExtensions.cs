6dbea74a03340ab981aff29ba1f21ca9ce53803c
Andrey Ordynsky <andreyordynsky@gmail.com>
2023-10-28 00:00:48 -0700

    API calculate age fix

--- API/Extensions/DateTimeExtensions.cs
+++ API/Extensions/DateTimeExtensions.cs
@@ -5,9 +5,8 @@ public static class DateTimeExtensions
     public static int CalculateAge(this DateOnly dateOnly) 
     {
         var today = DateOnly.FromDateTime(DateTime.UtcNow.Date);
-        var age  = today.AddDays(-dateOnly.Day);
-        age = age.AddMonths(-dateOnly.Month);
-        age = age.AddYears(-dateOnly.Year);
-        return age.Year;
+        var age = today.Year - dateOnly.Year;
+        if (dateOnly.AddYears(age) > today) age--;
+        return age;
     }
 }
